208977a205aa57bbe6e9c078dd9a7582f4a707ed
naveen-krk <125631978+naveen-krk@users.noreply.github.com>
2023-02-25 10:06:48 +0530

    Update ThreadStart.java

--- Exercise_3/ThreadStart.java
+++ Exercise_3/ThreadStart.java
@@ -1,18 +1,17 @@
 package Exercise_3;
-
 public class ThreadStart {
-public static void main(String[] args) {
-	Thread t=new Thread();
-	t.setName("Naveen Thread");
-	t.start();
-	t.start();
-	//The thread whose work is completed will get destroyed ,therefore 
-	//can not be started again.
+    public static void main(String[] args) {
+        Thread t = new Thread();
+        t.setName("Naveen Thread");
+        t.start();
+        t.start();
+        //The thread whose work is completed will get destroyed ,therefore 
+        //can not be started again.
+    }
 }
-}
-class CreateThread implements Runnable{
-	@Override
-	public void run() {
-		System.out.println("Thread name is"+Thread.currentThread());
-	}
+class CreateThread implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Thread name is" + Thread.currentThread());
+    }
 }
