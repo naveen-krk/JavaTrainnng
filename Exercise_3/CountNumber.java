779ab94e3a93e2f9b267bb079e43263398b6b12e
naveen-krk <125631978+naveen-krk@users.noreply.github.com>
2023-02-28 20:25:47 +0530

    Update CountNumber.java

--- Exercise_3/CountNumber.java
+++ Exercise_3/CountNumber.java
@@ -3,17 +3,39 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 public class CountNumber {
 public static void main(String[] args) {
-	ExecutorService es=Executors.newFixedThreadPool(1);
+	CountNumber number =new CountNumber();
+	ExecutorService es=Executors.newFixedThreadPool(2);
 	System.out.println("Counting Numbers");
 	es.execute(()->{
-		for(int i=1;i<=100;i++) {
-			try {
-				Thread.sleep(1000);
-				System.out.println(i);
-			}catch(Exception e) { }  
-			if(i%10==0)
-				System.out.println("Multiple of 10");
+		try {
+			number.num();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	});
+	es.execute(()->{
+		try {
+			number.mul();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	});
+}
+synchronized void num() throws InterruptedException {
+	for(int i=1;i<=100;i++) {
+		if(i%10!=0)	
+			System.out.println(i);
+		else {
+			notify();
+			wait();
+		}
+		}
+	}
+synchronized void mul() throws InterruptedException {	
+System.out.println("Multiple of 10");
+notify();
+wait();
 }
 }
